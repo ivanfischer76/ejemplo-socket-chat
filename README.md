05091adf68ed66642e3e8fb9ca9f8c2c4a80b8fe
Iván Fischer <ivanfischer76@gmail.com>
2021-04-26 20:15:01 -0300

    commit final-06/04/2021

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Este es un pequeño servidor de express listo para ejecutarse y servir la carpeta public en la web.
 
-Es un ejemplo de chat con salas utilizando sockets en NodeJs.
+Es un ejemplo de chat con salas utilizando sockets en NodeJs. Es una buena base funcional como para empezar a personalizar y ampliar funcionalidades para cear una estupenda sala de chat.
 
 Descargar y Ejecutar ```npm install``` para cargar los módulos de node. 
 
-También debe renombrarse .example.env a .env y colocar el puerto, que en este momento es 3000 pero puede ser cualquier otro.
+También debe renombrarse ```.example.env``` a ```.env``` y colocar el puerto, que en este momento es 3000 pero puede ser cualquier otro.
--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,25 +1,28 @@
-
-
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
 
-if(!params.has('nombre') || !params.has('sala')){
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
+
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
-    socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
-    });
-});
 
+    socket.emit('entrarChat', usuario, function(resp) {
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
+    });
+
+});
 
 // escuchar
 socket.on('disconnect', function() {
@@ -31,6 +34,7 @@ socket.on('disconnect', function() {
 
 // Enviar información
 // socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -38,15 +42,21 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-    console.log('Servidor:', mensaje);
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar cuando un usuario entra o sale del chat
-socket.on('listaPersonas', function(personas) {
-    console.log(personas);
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+    // console.log(personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje Privado', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
